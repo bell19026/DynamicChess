661bfd106cab85ef698ae2dc1196b503476ab22b
Liam <Bell1@LAPTOP-FIQ0I789.(none)')>
2020-03-12 05:23:18 -0400

    - Created the Board and Some Package Organization

--- src/main/java/com/liambell/dynamicchess/DynamicchessApplication.java
+++ src/main/java/com/liambell/dynamicchess/DynamicchessApplication.java
@@ -4,10 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DynamicchessApplication {
+public class DynamicChessApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DynamicchessApplication.class, args);
+    public static void main(String[] args) {SpringApplication.run(DynamicChessApplication.class, args);
     }
 
 }
+
+/*
+Model - Logic. Classes that are used to assign properties to things.
+View - Front End or Web Browser
+Controller - Mediator between Model and the View
+
+View -> Hey controller, I moved a chess piece from E3 to G5;
+Controller -> Hey Model, I just got this information about the View moving a piece from E3 to G5;
+Model -> Hold up, lemme do some math. I'll get back to you.
+Model -> here is my updated information from the back.
+Controller -> Hey View, my main man Model just gave me this.
+View -> Hey browser, show this information that is current.
+
+ */
