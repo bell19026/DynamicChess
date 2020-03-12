0f284d5399153fd9e7e1bbe6e41b7da95e28546d
Liam <Bell1@LAPTOP-FIQ0I789.(none)')>
2020-03-12 04:28:12 -0400

    - Initial Commit

---
+++ src/main/java/com/liambell/dynamicchess/DynamicchessApplication.java
@@ -0,0 +1,13 @@
+package com.liambell.dynamicchess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicchessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicchessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liambell/dynamicchess/DynamicchessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liambell.dynamicchess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicchessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
